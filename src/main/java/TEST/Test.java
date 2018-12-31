eb0f15ee7bde41f2efd9aab21f1d7ff32e2b635a
hamzaenaime <hamza.enaime@outlook.com>
2018-12-31 18:20:04 +0100

    create account

---
+++ src/main/java/TEST/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TEST;
+
+import DAO.Dao;
+
+/**
+ *
+ * @author hamza
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Dao dao = new Dao();
+
+    }
+}
