c4e73dbf1f6c49a84702767a21391247a52de4c0
TaoufikXx <taoufik.ajaanit.1@gmail.com>
2019-01-09 18:17:10 +0100

    adjust design

--- src/main/java/VUES/dashboardapp/releve.java
+++ src/main/java/VUES/dashboardapp/releve.java
@@ -108,11 +108,11 @@ public class releve extends javax.swing.JPanel{
                 .addComponent(jXDatePicker1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(412, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(168, 168, 168)
+                .addGap(159, 159, 159)
                 .addComponent(btnGenerate)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btnGenerate1)
-                .addGap(108, 108, 108))
+                .addGap(118, 118, 118))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
