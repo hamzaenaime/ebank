fe36b3eff75d2209d899542722e085f262fa420a
enaime hamza <hamza.enaime@outlook.com>
2019-01-30 19:13:19 +0100

    add connection failed jframe

--- src/main/java/DAO/Dao.java
+++ src/main/java/DAO/Dao.java
@@ -17,27 +17,20 @@ public class Dao {
     private static String username = "krvxwoxrqrqrlv";
     private static String password = "f3b61448d29137faef397b3f495a4dacd8662a95fbe070df8b7a4ea4dfdc2410";
     private static Connection connection;
-
+    
     public Dao() {
         try {
             System.out.println("connection...");
             connection = DriverManager.getConnection(url, username, password);
             System.out.println("every thing is ok *_* !!! ");
         } catch (SQLException ex2) {
-            /*System.out.println("switched to local database server");
-            try {
-                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ebank", "postgres", "bilal123");
-                System.out.println("every thing is ok *_* !!! ");
-            }catch (SQLException ex) {
-                System.err.println("problem de connection  " + ex.getMessage());
-            }*/
-            System.out.println("problem de connection  !! " + ex2.getMessage());
+            new VUES.ConnectionFailed().setVisible(true);
         }
-
+        
     }
-
+    
     public static Connection getConnection() {
         return connection;
     }
-
+    
 }
